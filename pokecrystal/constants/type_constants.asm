0fde2e0a6e9ed75375b839ac13efe7208c89d720
Sharath Hebbar <sharathhebbar24@gmail.com>
2020-09-27 20:58:06 +0530

    poke-crystal-revamped
    poke-crystal-revamped

---
+++ pokecrystal/constants/type_constants.asm
@@ -0,0 +1,40 @@
+; TypeNames indexes (see data/types/names.asm)
+; also used in:
+; - PokedexTypeSearchConversionTable (see data/types/search_types.asm)
+; - PokedexTypeSearchStrings (see data/types/search_strings.asm)
+; - TypeMatchups (see data/types/type_matchups.asm)
+; - TypeBoostItems (see data/types/type_boost_items.asm)
+	const_def
+
+	const NORMAL
+	const FIGHTING
+	const FLYING
+	const POISON
+	const GROUND
+	const ROCK
+	const BIRD
+	const BUG
+	const GHOST
+	const STEEL
+
+UNUSED_TYPES EQU const_value
+	const_next 19
+	const CURSE_TYPE
+UNUSED_TYPES_END EQU const_value
+
+	const FIRE
+	const WATER
+	const GRASS
+	const ELECTRIC
+	const PSYCHIC_TYPE
+	const ICE
+	const DRAGON
+	const DARK
+TYPES_END EQU const_value
+
+NUM_TYPES EQU TYPES_END + UNUSED_TYPES - UNUSED_TYPES_END
+
+TYPE_MASK EQU %00111111
+PHYSICAL  EQU %01000000
+SPECIAL   EQU %10000000
+STATUS    EQU %11000000
---
+++ pokecrystal/engine/events/checkforhiddenitems.asm
@@ -0,0 +1,117 @@
+CheckForHiddenItems:
+; Checks to see if there are hidden items on the screen that have not yet been found.  If it finds one, returns carry.
+	call GetMapScriptsBank
+	ld [wBuffer1], a
+; Get the coordinate of the bottom right corner of the screen, and load it in wBuffer3/wBuffer4.
+	ld a, [wXCoord]
+	add SCREEN_WIDTH / 4
+	ld [wBuffer4], a
+	ld a, [wYCoord]
+	add SCREEN_HEIGHT / 4
+	ld [wBuffer3], a
+; Get the pointer for the first bg_event in the map...
+	ld hl, wCurMapBGEventsPointer
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+; ... before even checking to see if there are any BG events on this map.
+	ld a, [wCurMapBGEventCount]
+	and a
+	jr z, .nobgeventitems
+; For i = 1:wCurMapBGEventCount...
+.loop
+; Store the counter in wBuffer2, and store the bg_event pointer in the stack.
+	ld [wBuffer2], a
+	push hl
+; Get the Y coordinate of the BG event.
+	call .GetFarByte
+	ld e, a
+; Is the Y coordinate of the BG event on the screen?  If not, go to the next BG event.
+	ld a, [wBuffer3]
+	sub e
+	jr c, .next
+	cp SCREEN_HEIGHT / 2
+	jr nc, .next
+; Is the X coordinate of the BG event on the screen?  If not, go to the next BG event.
+	call .GetFarByte
+	ld d, a
+	ld a, [wBuffer4]
+	sub d
+	jr c, .next
+	cp SCREEN_WIDTH / 2
+	jr nc, .next
+; Is this BG event a hidden item?  If not, go to the next BG event.
+	call .GetFarByte
+	cp BGEVENT_ITEM
+	jr nz, .next
+; Has this item already been found?  If not, set off the Itemfinder.
+	ld a, [wBuffer1]
+	call GetFarHalfword
+	ld a, [wBuffer1]
+	call GetFarHalfword
+	ld d, h
+	ld e, l
+	ld b, CHECK_FLAG
+	call EventFlagAction
+	ld a, c
+	and a
+	jr z, .itemnearby
+
+.next
+; Restore the bg_event pointer and increment it by the length of a bg_event.
+	pop hl
+	ld bc, BG_EVENT_SIZE
+	add hl, bc
+; Restore the BG event counter and decrement it.  If it hits zero, there are no hidden items in range.
+	ld a, [wBuffer2]
+	dec a
+	jr nz, .loop
+
+.nobgeventitems
+	xor a
+	ret
+
+.itemnearby
+	pop hl
+	scf
+	ret
+
+.GetFarByte:
+	ld a, [wBuffer1]
+	call GetFarByte
+	inc hl
+	ret
+
+RockItemEncounter:
+	ld hl, .RockItems
+	call Random
+.loop
+	sub [hl]
+	jr c, .ok
+rept 2
+	inc hl
+endr
+	jr .loop
+.ok
+	ld a, [hli]
+	cp $ff
+	ld a, NO_ITEM
+	jr z, .done
+	ld a, [hli]
+.done
+	ld [wScriptVar], a
+	ret
+	
+.RockItems:
+	db 1, MAX_REVIVE
+	db 2, THICK_CLUB
+	db 4, NUGGET
+	db 6, STAR_PIECE
+	db 12, BIG_PEARL
+	db 18, ETHER
+	db 24, HARD_STONE
+	db 24, SOFT_SAND
+	db 48, PEARL
+	db 64, BRICK_PIECE
+	db 48, NO_ITEM
+	db -1
